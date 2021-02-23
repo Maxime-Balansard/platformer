bf10a207106806179053649c99831b9835618c79
maxime <maxime.balansard@hotmail.fr>
2021-02-23 19:00:39 +0100

    new

--- src/scenes/TableauPixelart.js
+++ src/scenes/TableauPixelart.js
@@ -147,9 +147,9 @@ class TableauPixelart extends Tableau{
         new MonsterYokai(this,400,300);
 
         //1er tableau 
-       new Oni(this,200,416);
+       //new Oni(this,200,416);
        //2eme tableau
-       new Oni(this,1100,416);
+       //new Oni(this,1100,416);
        new MonsterFly(this,1100,200)
        new MonsterFly(this,1200,100)
       
